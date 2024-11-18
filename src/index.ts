5ebbc302c1a6f04c7b7125a872dbc15f11085b79
Đoàn Phương Nam <120766900+DPhngNam@users.noreply.github.com>
2024-11-18 16:42:45 +0700

    fix: port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 import mongoService from './services/mongo.service'
 import router from './routes'
 const app: Express = express()
-const PORT: number = 8000
+const PORT = process.env.PORT || 3000;
 
 dotenv.config()
 app.use(json())
