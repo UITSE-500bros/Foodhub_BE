15dfe48b20202d473f3443eec0200b668072f815
DPhngNam <22520908@gm.uit.edu.vn>
2024-12-24 21:45:41 +0700

    fix: get product's detail

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import {categoryController} from "../controllers";
 
 const categoryRouter = Router();
-categoryRouter.get("/", categoryController.getAllCategories);
+categoryRouter.get("/",categoryController.getAllCategories);
 categoryRouter.post("/", categoryController.createCategory);
 export default categoryRouter;
