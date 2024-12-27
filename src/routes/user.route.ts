9ba99a982c6c22df0f8d6343b9463c950c6e15f6
DPhngNam <22520908@gm.uit.edu.vn>
2024-12-27 21:55:02 +0700

    Update user.route.ts

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -9,6 +9,6 @@ userRouter.post("/favorites",authMiddleware, userController.addFavorite);
 userRouter.patch("/profile",authMiddleware, userController.updateProfile);
 userRouter.post("/payment", userController.createPaymentUrl);
 userRouter.get("/payment/callback", userController.getReturn);
-userRouter.get("/google", userController.loginGoogle);
+userRouter.post("/google", userController.loginGoogle);
 userRouter.get("/callback", userController.redirectCallback);
 export default userRouter;
