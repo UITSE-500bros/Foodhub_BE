1904a397a81d4f6256ddd95177011ae229061acc
DPhngNam <22520908@gm.uit.edu.vn>
2025-01-02 21:52:30 +0700

    change routes

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,13 +3,11 @@ import { userController } from "../controllers";
 import { authMiddleware } from "../middlewares";
 const userRouter = Router();
 
-userRouter.get("/favorites", userController.getFavorites);
-userRouter.post("/favorites",authMiddleware, userController.addFavorite);
-userRouter.patch("/profile",authMiddleware, userController.updateProfile);
 userRouter.get("/google", userController.loginGoogle);
 userRouter.get("/handlecallback", userController.redirectCallback);
 userRouter.post("/refreshToken", userController.retrieveSession);
-
-
+userRouter.get("/favorites", authMiddleware,userController.getFavorites);
+userRouter.post("/favorites",authMiddleware, userController.addFavorite);
+userRouter.patch("/profile",authMiddleware, userController.updateProfile);
 
 export default userRouter;
