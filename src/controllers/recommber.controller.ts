fbce4c6991c1449bab9f1f3d5e174d522d8c60bc
DPhngNam <22520908@gm.uit.edu.vn>
2025-01-14 00:32:58 +0700

    Update recommber.controller.ts

--- src/controllers/recommber.controller.ts
+++ src/controllers/recommber.controller.ts
@@ -10,7 +10,6 @@ const client = new ApiClient(
 );
 
 
-
 class RecommbeeController {
     async getRecommendations(req: AuthenticatedRequest, res: Response) {
         const response = await client.send(new requests.RecommendItemsToUser("e343107d-e15b-482c-9430-e34395928907", 5));
